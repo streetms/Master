97119ee2543ac30b30d934380ecebc21e65ce3b6
streetms <konstantin.isakov.2003@mail.ru>
2022-12-04 21:33:18 +0700

    init

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <boost/asio.hpp>
+#include <iostream>
+using namespace boost::asio;
+
+int main() {
+    boost::asio::io_service service;
+    boost::asio::ip::tcp::socket sock(service);
+    ip::tcp::acceptor acceptor(service, ip::tcp::endpoint(ip::tcp::v4(), 8005));
+    acceptor.accept(sock);
+    std::cout << "новое подключение\n";
+    while (true) {
+        std::string command;
+        std::getline(std::cin,command);
+        command += char(-1);
+        sock.write_some(boost::asio::buffer(command));
+    }
+}
