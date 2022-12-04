888ce3660825f4df0a54e5cfb102efb2d518b250
streetms <konstantin.isakov.2003@mail.ru>
2022-12-04 22:15:52 +0700

    adasfadgaddshs

--- main.cpp
+++ main.cpp
@@ -23,5 +23,6 @@ int main() {
         command[0] = 0;
         size_t bytes = read(sock, boost::asio::buffer(command), boost::bind(read_complete,command,_1,_2));
         std::cout << command;
+        command[0] = 0;
     }
 }
